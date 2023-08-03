9385790ad9f39c75aa9ac009cb749b5c68df2dfa
Charlie Machado <charlie.machado@stern.nyu.edu>
2023-08-03 18:56:26 -0400

    Created Navigable links on web app - render html pages
    Not getting header on 'hello'

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -6,13 +6,13 @@ home_routes = Blueprint("home_routes", __name__)
 @home_routes.route("/home")
 def index():
     print("HOME...")
-    return "Welcome Home"
+    return render_template('home.html')
     #return render_template("home.html")
 
 @home_routes.route("/about")
 def about():
     print("ABOUT...")
-    return "About Me"
+    return render_template('about.html')
     #return render_template("about.html")
 
 @home_routes.route("/hello")
